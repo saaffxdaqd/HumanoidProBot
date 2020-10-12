41f99ad95d912eeb629c717d616aae2c98d8bbba
VILLAIN <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-10-12 19:29:30 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -130,7 +130,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("Yo,🧞‍♂️I'm Alive..I'm a super bot 🔥...🚴‍♂️Speed 1 THz..Memory 1 Zettabyte.🕺🏻")
+        update.effective_message.reply_text("Hello World Am 𝕮𝖍𝖎𝖙𝖙𝖎 Reloaded Version 2.0 🔥...🚴‍♂️Speed 1 THz..Memory 1 Zettabyte.🕺🏻")
 
 def send_start(bot, update):
     #Try to remove old message
