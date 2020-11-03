7b24e268ae6c9ac340efb75b747693af5fc0f7c5
VILLAIN <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-11-04 01:10:19 +0530

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -69,7 +69,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⏪", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🏕", callback_data="bot_start"),
+                EqInlineKeyboardButton("🪂", callback_data="bot_start"),
              EqInlineKeyboardButton("⏩", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
