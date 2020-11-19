2554da7d06906e6bf5d3bb82639014d5d21298ff
ʄʀǟռᏦɛռֆ⊺ɛɨռ™ ❬❬ઝ¡ทg_૨¡ρ⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-11-19 18:01:29 +0530

    Update panel_string.py

--- tg_bot/modules/helper_funcs/panel_string.py
+++ tg_bot/modules/helper_funcs/panel_string.py
@@ -1,4 +1,4 @@
-ALLUKA_IMG = "https://telegra.ph/file/f07e38b26890e2fceb9fe.jpg"
+ALLUKA_IMG = "https://telegra.ph/file/a724e553fa32813a79318.jpg"
 
 SOURCE_STRING = """
 I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/DONOFTELEGRAM/HumanoidProBot)
@@ -9,13 +9,13 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /source: Information about my source.
- - /settings:
-  - in PM: will send you your settings for all supported modules.
-  - in a group: will redirect you to pm, with all that chat's settings.
+ -💫 /start: start the bot
+ -💫 /help: PM's you this message.
+ -💫 /help <module name>: PM's you info about that module.
+ -💫 /source: Information about my source.
+ -💫 /settings:
+  -💫 in PM: will send you your settings for all supported modules.
+  -💫 in a group: will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """
